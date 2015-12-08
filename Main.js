167cc96ba1edb1f4578006801cf820f6365b610a
lazarevic_tadija <lazarevic.tadija@gmail.com>
2015-12-08 15:48:55 +0100

    Some changes in event listeners. Now there can be multiple actions i.e. toogleButton or closeButton and openButton

--- Main.js
+++ Main.js
@@ -7,33 +7,17 @@ var clientCloseBtn = document.getElementById('clientCloseBtn');
 
 var x = new Menu();
 x.Init({
-    mask    : mask,
-    wrapper : clientWrapper,
-    menu    : clientMenu,
-    closeBtn: clientCloseBtn
-});
+    mask        : mask,
+    wrapper     : clientWrapper,
+    menu        : clientMenu,
+    closeButton    : clientCloseBtn,
+    openButton     : clientButton,
+//    toggleButton: clientButton,
+    onOpen      : function (params) {
+        clientButton.style.position = 'absolute';
+        clientButton.style.zIndex   = 1001;
+    },
+    onClose     : function (params) {
 
-clientButton.addEventListener('click', function (e) {
-    e.preventDefault();
-    x.Open();
-});
-
-// WORKER
-var workerButton   = document.getElementById('workerButton');
-var workerWrapper  = document.getElementById('workerWrapper');
-var workerMenu     = document.getElementById('workerMenu');
-var workerCloseBtn = document.getElementById('workerCloseBtn');
-
-var workerMenuObj = new Menu;
-workerMenuObj.Init({
-    mask    : mask,
-    wrapper : workerWrapper,
-    menu    : workerMenu,
-    closeBtn: workerCloseBtn
-});
-
-
-workerButton.addEventListener('click', function (e) {
-    e.preventDefault();
-    workerMenuObj.Open();
+    }
 });
--- Menu.js
+++ Menu.js
@@ -1,18 +1,37 @@
 var Menu = (function () {
-    var settings        = {};
+    var settings = {};
 
     function InitEvents() {
-        // Event for clicks on the close button inside the menu.
-        settings.closeBtn.addEventListener('click', function (e) {
-            e.preventDefault();
-            Close();
-        }.bind(this));
+        // Event for clicks on the close button inside the menu, if the button for close and open are the same
+        if (settings.toggleButton) {
+            settings.toggleButton.addEventListener('click', function (e) {
+                if (Utils.HasClass(settings.mask, 'isActive')) {
+                    e.preventDefault();
+                    Close();
+                }
+                else if (!Utils.HasClass(settings.mask, 'isActive')) {
+                    e.preventDefault();
+                    Open();
+                }
+            });
+        }
+        else if (settings.openButton && settings.closeButton) {
+            settings.openButton.addEventListener('click', function (e) {
+                e.preventDefault();
+                Open();
+            });
+            settings.closeButton.addEventListener('click', function (e) {
+                e.preventDefault();
+                Close();
+            });
+        }
+
 
         // Event for clicks on the mask.
         settings.mask.addEventListener('click', function (e) {
             e.preventDefault();
             Close();
-        }.bind(this));
+        });
     }
 
     function Close() {
@@ -20,22 +39,34 @@ var Menu = (function () {
         Utils.RemoveClassWithName(settings.wrapper, 'hasActiveMenu');
         Utils.RemoveClassWithName(settings.menu, 'isActive');
         Utils.RemoveClassWithName(settings.mask, 'isActive');
+        // Callback
+        if (settings.onClose) {
+            settings.onClose();
+        }
+    }
+
+    function Open() {
+        Utils.AddClass(settings.body, 'hasActiveMenu');
+        Utils.AddClass(settings.menu, 'isActive');
+        Utils.AddClass(settings.mask, 'isActive');
+        // Callback
+        if (settings.onOpen) {
+            settings.onOpen();
+        }
     }
 
     return {
         Init: function (params) {
-            settings.wrapper  = params.wrapper;
-            settings.mask     = params.mask;
-            settings.menu     = params.menu;
-            settings.body     = document.body;
-            settings.closeBtn = params.closeBtn;
+            settings.body         = document.body;
+            settings.wrapper      = params.wrapper;
+            settings.mask         = params.mask;
+            settings.menu         = params.menu;
+            settings.closeButton  = params.closeButton;
+            settings.openButton   = params.openButton;
+            settings.toggleButton = params.toggleButton;
+            settings.onOpen       = params.onOpen || null;
+            settings.onClose      = params.onClose || null;
             InitEvents();
-        },
-        Open: function () {
-            Utils.AddClass(settings.body, 'hasActiveMenu');
-            Utils.AddClass(settings.menu, 'isActive');
-            Utils.AddClass(settings.mask, 'isActive');
-
 
         }
     }
